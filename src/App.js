944f6c336b342a9450d3237a6d3f5c46c7cce0aa
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-06-27 18:58:16 +0530

    showing message after increase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(0);
+
+  useEffect(() => {
+    alert("iam Increased");
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          setNum(num + 1);
+        }}
+      >
+        {" "}
+        Click me {num}
+      </button>
     </div>
   );
 }
